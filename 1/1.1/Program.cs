32ad1bb2330b9fffe7669c3bcb8461d1dd53d2ec
Елена <Elenacozmos@bk.ru>
2022-09-23 19:06:43 +0300

    Доработки

--- 1/1.1/Program.cs
+++ 1/1.1/Program.cs
@@ -12,9 +12,9 @@ void CheckingNumber(string number)
 {
   if (number[0]==number[4] || number[1]==number[3])
   {
-    Console.WriteLine($"Ваше число: {number} - палиндром.");
+    Console.WriteLine($"Ваше число: {number} - палиндром");
   }
-  else Console.WriteLine($"Ваше число: {number} - не палиндром.");
+  else Console.WriteLine($"Ваше число: {number} - не палиндром");
 }
 
 if (number!.Length == 5)
